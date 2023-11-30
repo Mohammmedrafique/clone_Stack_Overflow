4e82e2ac9316359dadeaa909c54a6d8b181b3b81
Mohammmedrafique <pushpendrasharmakarauli@gmail.com>
2023-11-30 19:30:52 +0530

    d

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,8 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
+const cros = require("cors");
+server.use(cros());
 server.use(middlewares);
 server.use(router);
 server.listen(8000, () => {
